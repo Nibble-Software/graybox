70a0bc91293ad65f7e40694bd18af64d425cb46b
chelo154 <chelo154@gmail.com>
2022-06-20 20:59:50 -0300

    Forcing Import

--- src/graybox/graybox.py
+++ src/graybox/graybox.py
@@ -29,7 +29,11 @@ def evaluate_gray_box(solution, function_name, args, expected):
     check_file_existence(f'{solution_file_path}.py')
 
     try:
-        package = __import__(module_path, locals(), globals(), fromlist=[package_name])
+        package, imported = force_import(module_path, package_name)
+
+        while not imported:
+            package, imported = force_import(module_path, package_name)
+
         function = getattr(package, function_name)
 
         if not callable(function):
@@ -64,3 +68,9 @@ def evaluate_gray_box(solution, function_name, args, expected):
         os.remove(f'{path}{separator}{module_name}.py')
 
 
+def force_import(module_path, package_name):
+    try:
+        package = __import__(module_path, locals(), globals(), fromlist=[package_name])
+        return package, True
+    except ModuleNotFoundError as error:
+        return None, False
